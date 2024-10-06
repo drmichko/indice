b23098c1fc833c30f5f0a86bb6f8a21c5a340a8e
drmichko <langevin@univ-tln.fr>
2024-10-06 20:09:46 +0200

    bug?

--- final.c
+++ final.c
@@ -6,6 +6,58 @@
 #include "option.h"
 #include "degrees.h"
 
+
+int tfr[ 256 ];
+
+typedef struct list {
+        int * fct;
+        vector key;
+        struct list * next;
+} enrlist, *list;
+
+list  lf[ 4 ] = {NULL}, result;
+
+
+void freelist( list l )
+{
+while ( l ){
+        list aux = l;
+        l = l->next;
+        free( aux->fct);
+        free( aux );
+}
+}
+
+
+void print( int f[], int q )
+{
+printf("\nfct:");
+int t;
+for( t = 0; t < q; t++ )
+	printf(" %d", f[t] );
+printf("\n");
+}
+
+void plist( list l, int q  )
+{
+while ( l ){
+        list aux = l;
+        l = l->next;
+        print( aux->fct, q );
+}
+}
+
+void push32( int f[] )
+{ list aux;
+  aux = malloc( sizeof(enrlist) );
+  aux->fct = calloc( 32, sizeof(int ) ); 
+  int t;
+  for( t = 0; t < 32; t++ )
+  	aux->fct[t] = f[ t ];
+  aux->next = result;
+  result = aux;
+}
+
 int subsize = 128;
        #define _GNU_SOURCE         /* Consultez feature_test_macros(7) */
        #include <search.h>
@@ -17,120 +69,143 @@ int subsize = 128;
 listspace lsp = NULL;
 
 
-int Soluce;
 int soluce;
-
-
-int value[ 9 ][256];
-
-void mkvalue( void )
-{
-int r;
-for( r = 4 ; r < 9; r++ ){
-	int v;
-	for( v = 0; v < 256; v++ )
-		value[ r ][ v ] = 0;
-	int q = 1 << ( 8 - r );
-	int p;
-	for( p = 0; 2*p <= q; p++ )
-		value[ r ][(q - 2*p)  *  ( 1 << (r-4) )] =  1;
-}
-}
-int accept( int r , int v )
+int accept32(  int v )
 {
 v = abs( v );
-return  ( v < 256  &&  value[ r ][ v ] );
+return  ( v == 0 || v == 4 || v == 8 || v == 12 || v == 16 );
 }
 
-int admis( int s, int r, int tfr[]    )
+int accept64(  int v )
+{
+v = abs( v );
+return  ( v == 0 ||  v == 8 || v == 16 );
+}
+
+int admis32( int fct[] )
 { int f[ 256 ] = { 0 };
-  int t, q = 1 << r;
-  
-  for( t = 0; t < q; t++ )
-	  f[ t ] = tfr[ s +  t];
+  int t;
+  for( t = 0; t < 32; t++ )
+	  f[ t ] = fct[ t ];
 
-  // printf("\nadmis %d :", r ); for( t = 0; t < 256 ; t++ ) printf("%3d", tfr[t] ); printf("\n");
-  Fourier( f,  q   );
-
-  //printf("\ntfr %d :", r ); for( t = 0; t < q ; t++ ) printf("%3d", f[t] ); printf("\n");
-  //for( t = 0; t < q; t++ ) printf(" %3d", accept( r, f[t] ) ); printf("\n");
+  Fourier( f,  32   );
   
-  for( t = 0; t < q; t++ )
-	 if ( ! accept( r, f[ t ] ) ) {
-		 //printf("\n#bad at %d :  %d\n", r, f[t] );
+  for( t = 0; t < 32 ; t++ )
+	 if ( ! accept32(  f[ t ] ) ) {
 		 return 0;
 	 }
   return 1;
 }
 
-void filling( int s , int r, int tfr[256]   )
+
+list  mkblock ( int offset  )
 { 
   int t;
-  int pos[ 256 ] ={ 0 }, nbp = 0;
-  int q = 1 << r;
-  for( t = 0; t < q ; t++ ){
-	  if ( tfr[ t + s  ] == 0 ) {
-		  pos[ nbp++ ] = t + s;
-	  }
+  int pos[ 32 ] ={ 0 }, nbp = 0;
+  int left[ 32 ] = {0}, right[ 32 ] = {0};
+  for( t = 0; t < 32 ; t++ ){
+	  if ( tfr[ t + offset  ] == 0 ) {
+		  pos[ nbp++ ] = t ;
+	  } else  left[t] = tfr[ t + offset ]; 
+          right[ t ] = left[ t ];
   }
+  soluce = 0;
+  result = NULL;
   int i, p;
-
-  	for( p = 0; p < (1 << nbp) ; p++ ) {
-     		for( i = 0; i < nbp; i++ ){
-	     		if ( ( 1 <<i ) & p ) 
-			      tfr[ pos[ i ] ] = 1;
-	     		else  tfr[ pos[ i ] ] = -1;
-	     	tfr [ pos[ i ] + 128  ] =  - tfr[ pos[ i ] ];
+  for( p = 0; p < (1 << nbp) ; p++ ) {
+     	for( i = 0; i < nbp; i++ ){
+	     	if ( ( 1 <<i ) & p ) 
+			      left[ pos[ i ] ] = 1;
+	     		else  left[ pos[ i ] ] = -1;
+	     	right [ pos[ i ]  ] =  - left[ pos[ i ] ];
      		}
-     		if ( admis( s - q  , 6  , tfr)  &&  admis( 128 + s - q  , 6 , tfr ) )  
-			  soluce++;
-     }
+     	if ( admis32( left )  &&  admis32( right  ) )  {
+			 push32( left );
+			 soluce++;
+		}
+	}
+printf(" %d", soluce );
+return result;
 }
-void search( int s , int r, int tfr[256]   )
-{ 
+
+int admis64( int fct []  )
+{ int f[ 256 ] = { 0 };
   int t;
-  int pos[ 256 ] ={ 0 }, nbp = 0;
-  int q = 1 << r;
-  for( t = 0; t < q ; t++ ){
-	  if ( tfr[ t + s  ] == 0 ) {
-		  pos[ nbp++ ] = t + s;
-	  }
-  }
-  int i, p;
-  	for( p = 0; p < (1 << nbp) ; p++ ) {
-     		for( i = 0; i < nbp; i++ ){
-	     		if ( ( 1 <<i ) & p ) 
-			      tfr[ pos[ i ] ] = 1;
-	     		else  tfr[ pos[ i ] ] = -1;
-	     	tfr [ pos[ i ] + 128  ] =  - tfr[ pos[ i ] ];
-     		}
-     		if ( admis( s  , 5  , tfr)  &&  admis( 128 + s , 5, tfr ) )  
-			 filling( s + q, 5, tfr );
-		}
+  for( t = 0; t < 64; t++ )
+	  f[ t ] = fct[ t ];
+  Fourier( f,  64   );
+  for( t = 0; t < 64 ; t++ )
+	 if ( ! accept64(  f[ t ] ) ) {
+		 return 0;
+	 }
+  return 1;
 }
 
-void search64(  0, int tfr[256]   )
-{ 
-  search64( 0, tfr );
-     		}
-     		if ( admis( s  , 5  , tfr)  &&  admis( 128 + s , 5, tfr ) )  
-			 filling( s + q, 5, tfr );
-		}
+int check64( int offset, int fct []  )
+{ int f[ 256 ] = { 0 };
+  int t;
+  for( t = 0; t < 64; t++ )
+	  if( tfr[ t + offset ] )  f[ t ] = fct[ t ];
+          else f[ t ] = - fct[ t ];
+
+  Fourier( f,  64   );
+  for( t = 0; t < 64 ; t++ )
+	 if ( ! accept64(  f[ t ] ) ) {
+		 return 0;
+	 }
+  return 1;
+}
+
+void glue64( int offset, list ll, list lr)
+{
+list lx;
+int fct[64];
+soluce = 0;
+while ( ll ) {
+	int t;
+	for( t = 0; t < 32; t++ )
+                        fct[t] = ll->fct[t];
+	lx = lr;
+	while ( lx ) {
+		for( t = 0; t < 32; t++ )
+			fct[t + 32 ] = lx->fct[t];
+		if ( admis64( fct ) && check64(offset, fct) ) soluce++;
+		lx = lx -> next;
+	}
+	ll = ll-> next;
+}
+printf("\nglue 64 : %d", soluce );
 }
 int test( boole f )
 { 
   int t;
-  int tfr[ 256 ] = { 0 };
-  for( t =  0; t < ffsize; t++ )
+  for( t =  0; t < 128; t++ )
 	  tfr[ t ] = f[ t ] ? -1 : 1 ;
    Fourier( tfr, 128 );
    for( t = 0; t < 128; t++ )
 	if ( tfr[ t ] != 0 ) 
 		tfr[ t + 128 ] = tfr[t] = ( tfr[ t ] > 0 )   ? 1 : -1;
 
-   soluce   = 0; 
-   search( 0 , 64, tfr );
-   printf("\n#soluce=%d\n", soluce );
+   printf("\n");
+   lf[ 0 ] =  mkblock( 0 );
+   
+   printf("\n");
+   lf[ 1 ] =  mkblock( 32 );
+
+   glue64( 0, lf[0], lf[1] );
+   freelist( lf[0] );
+   freelist( lf[1] );
+   
+   printf("\n");
+   lf[ 2 ] =  mkblock( 64 );
+   
+   printf("\n");
+   lf[ 3 ] =  mkblock( 96 );
+
+   glue64( 64, lf[2], lf[3] );
+   freelist( lf[2] );
+   freelist( lf[3] );
+   
    return soluce > 0 ;
 }
 
@@ -147,13 +222,11 @@ int main(int argc, char *argv[])
     int num = 0, count = 0;
 
 
-    mkvalue( );
 
     while ((f = loadBoole(src))) {
 		if (  job == num % mode) {
 		      assert( isnearbent( f ) == 1 ); 
 		      if ( test( f ) ) {
-			      panf( stdout, f );
 			      count++;
 		      }
 	}
