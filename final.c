8cc3bbdc3c8b2632cea129f723e88cd2443f8f2d
drmichko <langevin@univ-tln.fr>
2024-10-06 07:52:00 +0200

    final.c

--- final.c
+++ final.c
@@ -17,7 +17,7 @@ int subsize = 128;
 listspace lsp = NULL;
 
 
-int Soluce[ 8 ];
+int Soluce;
 int soluce;
 
 
@@ -26,14 +26,14 @@ int value[ 9 ][256];
 void mkvalue( void )
 {
 int r;
-for( r = 0; r < 9; r++ ){
+for( r = 4 ; r < 9; r++ ){
 	int v;
 	for( v = 0; v < 256; v++ )
 		value[ r ][ v ] = 0;
 	int q = 1 << ( 8 - r );
 	int p;
 	for( p = 0; 2*p <= q; p++ )
-		value[ r ][(q - 2*p)  ] =  1;
+		value[ r ][(q - 2*p)  *  ( 1 << (r-4) )] =  1;
 }
 }
 int accept( int r , int v )
@@ -42,17 +42,19 @@ v = abs( v );
 return  ( v < 256  &&  value[ r ][ v ] );
 }
 
-int admis( int r, int tfr[]    )
+int admis( int s, int r, int tfr[]    )
 { int f[ 256 ] = { 0 };
   int t, q = 1 << r;
+  
   for( t = 0; t < q; t++ )
-	  f[t] = tfr[t];
+	  f[ t ] = tfr[ s +  t];
+
   // printf("\nadmis %d :", r ); for( t = 0; t < 256 ; t++ ) printf("%3d", tfr[t] ); printf("\n");
-  Fourier( f, 256 );
+  Fourier( f,  q   );
+
   //printf("\ntfr %d :", r ); for( t = 0; t < q ; t++ ) printf("%3d", f[t] ); printf("\n");
-  // for( t = 0; t < q; t++ ) printf(" %3d", bad( f[t], r ) ); printf("\n");
-  for( t = 0; t < q; t++ )
-	  f[ t ] =  f[ t ] / q;
+  //for( t = 0; t < q; t++ ) printf(" %3d", accept( r, f[t] ) ); printf("\n");
+  
   for( t = 0; t < q; t++ )
 	 if ( ! accept( r, f[ t ] ) ) {
 		 //printf("\n#bad at %d :  %d\n", r, f[t] );
@@ -61,66 +63,60 @@ int admis( int r, int tfr[]    )
   return 1;
 }
 
-int ctrl( int r, int tfr[]    )
-{ int f[ 256 ] = { 0 };
-  int t;
-  for( t = 0; t < 256; t++ )
-	  f[t] = tfr[ t ];
-  Fourier( f, 256 );
-  for( t = 0; t < 256; t++ )
-	  f[ t ] =  f[ t ] / 256;
-  for( t = 0; t < 256; t++ ) 
-	  if ( ! accept( 8, f[t] ) ) {
-		  //printf("\n#bad :  %d\n", f[t] );
-		  break;
-	  }
-  for( t = 0; t < 256; t++ )
-	 if ( ! accept( f[ t ], 8 ) ) return 0;
-  return 1;
-}
-void search( int r, int tfr[256]   )
+void filling( int s , int r, int tfr[256]   )
 { 
   int t;
-
-  if ( r == 6  ) {
-  	  //printf("at %d :", r ); for( t = 0; t < 256 ; t++ ) printf("%3d", tfr[t] ); printf("\n");
-	  if ( admis( 5, tfr ) )
-		  soluce++;
-	  return;
-  }
-  if ( r == 8  ) {
-  	  //printf("at %d :", r ); for( t = 0; t < 256 ; t++ ) printf("%3d", tfr[t] ); printf("\n");
-	  if ( ctrl( 8, tfr ) )
-		  soluce++;
-	  return;
-  }
+  int pos[ 256 ] ={ 0 }, nbp = 0;
   int q = 1 << r;
-  int pos[ 256] ={ 0 }, nbp = 0;
-  for( t = 0; t < q; t++ ){
-	  if ( tfr[ t  ] == 0 ) {
-		  pos[ nbp++ ] = t;
+  for( t = 0; t < q ; t++ ){
+	  if ( tfr[ t + s  ] == 0 ) {
+		  pos[ nbp++ ] = t + s;
 	  }
   }
   int i, p;
-  //printf("\nr=%d nbp=%d :", r, nbp );
-  //for( p = 0; p < nbp; p++ )
-//	  printf(" %d", pos[p] );
 
-  if ( nbp > 0 ){
   	for( p = 0; p < (1 << nbp) ; p++ ) {
      		for( i = 0; i < nbp; i++ ){
 	     		if ( ( 1 <<i ) & p ) 
-			      tfr[ pos[ i ] ] = 16;
-	     		else  tfr[ pos[ i ] ] = -16;
+			      tfr[ pos[ i ] ] = 1;
+	     		else  tfr[ pos[ i ] ] = -1;
 	     	tfr [ pos[ i ] + 128  ] =  - tfr[ pos[ i ] ];
      		}
-     		if ( admis( r , tfr) )  
-			search( r + 1, tfr );
+     		if ( admis( s - q  , 6  , tfr)  &&  admis( 128 + s - q  , 6 , tfr ) )  
+			  soluce++;
      }
-     for( i = 0; i < nbp; i++ ) tfr [ pos[ i ] + 128  ] = tfr[ pos[ i ] ]  = 0;
-  } else search( r + 1, tfr );
+}
+void search( int s , int r, int tfr[256]   )
+{ 
+  int t;
+  int pos[ 256 ] ={ 0 }, nbp = 0;
+  int q = 1 << r;
+  for( t = 0; t < q ; t++ ){
+	  if ( tfr[ t + s  ] == 0 ) {
+		  pos[ nbp++ ] = t + s;
+	  }
+  }
+  int i, p;
+  	for( p = 0; p < (1 << nbp) ; p++ ) {
+     		for( i = 0; i < nbp; i++ ){
+	     		if ( ( 1 <<i ) & p ) 
+			      tfr[ pos[ i ] ] = 1;
+	     		else  tfr[ pos[ i ] ] = -1;
+	     	tfr [ pos[ i ] + 128  ] =  - tfr[ pos[ i ] ];
+     		}
+     		if ( admis( s  , 5  , tfr)  &&  admis( 128 + s , 5, tfr ) )  
+			 filling( s + q, 5, tfr );
+		}
 }
 
+void search64(  0, int tfr[256]   )
+{ 
+  search64( 0, tfr );
+     		}
+     		if ( admis( s  , 5  , tfr)  &&  admis( 128 + s , 5, tfr ) )  
+			 filling( s + q, 5, tfr );
+		}
+}
 int test( boole f )
 { 
   int t;
@@ -130,11 +126,11 @@ int test( boole f )
    Fourier( tfr, 128 );
    for( t = 0; t < 128; t++ )
 	if ( tfr[ t ] != 0 ) 
-		tfr[ t + 128 ] = tfr[ t ];
+		tfr[ t + 128 ] = tfr[t] = ( tfr[ t ] > 0 )   ? 1 : -1;
 
-   soluce   = 0;
-   search( 2   , tfr);
-	  printf("\ns=%d\n", soluce );
+   soluce   = 0; 
+   search( 0 , 64, tfr );
+   printf("\n#soluce=%d\n", soluce );
    return soluce > 0 ;
 }
 
@@ -166,6 +162,5 @@ int main(int argc, char *argv[])
     }
     printf("\ncount=%d / %d\n", count, num );
    
-	  printf("\ns=%d\n", soluce );
     return 0;
 }
